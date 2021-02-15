cd2e07213d71724ac47a56990096d23aaa1e566a
Pramod N K <pramodnk7@gmail.com>
2021-02-16 02:24:57 +0530

    project all files commeited

---
+++ referal/sendemail.py
@@ -0,0 +1,28 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from .passcode import passcode
+
+
+def send_mail(subject, body, receiver_email):
+	try:
+	#The mail addresses and password
+		sender_address = passcode.sender_address
+		sender_pass = passcode.sender_pass
+		receiver_address = receiver_email
+		#Setup the MIME
+		message = MIMEMultipart()
+		message['From'] = sender_address
+		message['To'] = receiver_email
+		message['Subject'] = subject   #The subject line
+		message.attach(MIMEText(body, 'html'))
+		#Create SMTP session for sending the mail
+		session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
+		session.starttls() #enable security
+		session.login(sender_address, sender_pass) #login with mail_id and password
+		text = message.as_string()
+		session.sendmail(sender_address, receiver_address, text)
+		session.quit()
+		print('Mail Sent', flush=True)
+	except Exception as e:
+		print(e, flush=True)
